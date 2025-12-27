c8834e5cf5b8983f1d5ad9b0d617d9a2a3468721
Aviv R <aviv80010@gmail.com>
2025-12-27 19:17:26 +0200

    Enhance FramedBox component with customizable content frame styles and border properties

--- src/components/FramedBox/FramedBox.tsx
+++ src/components/FramedBox/FramedBox.tsx
@@ -5,13 +5,20 @@ interface FramedBoxProps {
   title?: string;
   frameColor?: "green" | "white";
   titleStyle?: "rounded" | "square";
+  contentFrameStyle?: "rounded" | "square";
 }
 
-export const FramedBox = ({ title, frameColor = "green", titleStyle = "rounded", children }: FramedBoxProps) => {
+export const FramedBox = ({
+  title,
+  frameColor = "green",
+  titleStyle = "rounded",
+  contentFrameStyle = "rounded",
+  children,
+}: FramedBoxProps) => {
   return (
-    <div className={`${styles["framed-box"]} ${styles[frameColor]} `}>
+    <div className={`${styles[frameColor]} ${styles["framed-box"]}`}>
       {title && <div className={`${titleStyle} ${styles["framed-box-title"]}`}>{title}</div>}
-      <div className={styles["framed-box-content"]}>{children}</div>
+      <div className={`${styles[contentFrameStyle]} ${styles["framed-box-content"]}`}>{children}</div>
     </div>
   );
 };
