7ce92c93c208a76d0f155fe3da779d9c73732adb
Aviv R <aviv80010@gmail.com>
2025-12-28 21:47:14 +0200

    Added game card exposing animations: -Added flip animation for the card when exposed -Add flame effect SVG and border for losing card -Added shine and scale animation for the winning card content

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -2,11 +2,27 @@ import styles from "./GameCard.module.css";
 import type { GameCard as GameCardType } from "../../types/game.types";
 import BombIcon from "../../assets/bomb_shadow.svg?react";
 import CrownIcon from "../../assets/crown_shadow.svg?react";
+import FlameEffect from "../../assets/flame_effect.svg?react";
 
 export const GameCard = ({ gameCard, onClick }: { gameCard: GameCardType; onClick: () => void }) => {
   return (
-    <div className={styles["game-card"]} onClick={onClick}>
-      {gameCard.isExposed ? <>{gameCard.isWinning ? <CrownIcon /> : <BombIcon />}</> : <></>}
+    <div className={`${styles["game-card-container"]}`}>
+      <div onClick={onClick} className={`${styles["game-card"]} ${gameCard.isExposed && styles["game-card-exposed"]}`}>
+        <div className={styles["game-card-front"]}></div>
+        <div
+          className={`${styles["game-card-back"]} ${gameCard.isWinning && gameCard.isExposed ? styles["game-card-winning"] : ""}`}
+        >
+          {gameCard.isExposed && (
+            <div
+              className={gameCard.isWinning ? styles["game-card-content-winning"] : styles["game-card-content-losing"]}
+            >
+              {gameCard.isWinning ? <CrownIcon /> : <BombIcon />}
+            </div>
+          )}
+        </div>
+      </div>
+      {/* This is a hack to get the losing card flame effect to work */}
+      <FlameEffect aria-hidden="true" style={{ width: 0, height: 0 }} />
     </div>
   );
 };
