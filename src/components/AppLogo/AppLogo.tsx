658b7462a6d22b3643ff6797c6a3e38ba4ab4807
Aviv R <aviv80010@gmail.com>
2025-12-29 23:46:32 +0200

    Changed logo to an image for better looks and responsiveness

--- src/components/AppLogo/AppLogo.tsx
+++ src/components/AppLogo/AppLogo.tsx
@@ -1,9 +1,10 @@
 import styles from "./AppLogo.module.css";
+import logoImage from "../../assets/graphics/logo.png";
 
 export const AppLogo = () => {
   return (
     <div className={styles["app-logo"]}>
-      <h1>LUCKYNMINER</h1>
+      <img src={logoImage} alt="Lucky Miner Logo" className={styles["logo-image"]} />
     </div>
   );
 };
