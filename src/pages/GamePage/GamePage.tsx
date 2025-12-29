cb8da6c4acad5580096774b9087481133e3235dc
Aviv R <aviv80010@gmail.com>
2025-12-29 19:31:13 +0200

    Implement game board footer with dynamic actions based on game status. Update GamePage component to handle game card exposure and balance calculations. Enhance CSS for footer layout.

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,19 +1,59 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { useGameBoard } from "../../hooks/useGameBoard";
 import styles from "./GamePage.module.css";
 import { AppLogo } from "../../components/AppLogo/AppLogo";
 import { FramedBox } from "../../components/FramedBox/FramedBox";
 import { GameBoard } from "../../components/GameBoard/GameBoard";
+import type { GameStatus } from "../../types/game.types";
+import { BaseButton } from "../../components/BaseButton/BaseButton";
 
 export const GamePage = () => {
-  const { gameBoard, loading, error, fetchGameBoard, setGameCardExposed } = useGameBoard();
-  const [balance] = useState(0);
+  const { gameBoard, loading, error, setGameCardExposed, resetGameBoard, exposedCards, fetchGameBoard } =
+    useGameBoard();
 
   const maxPrize = useMemo(() => {
     if (!gameBoard) return 0;
-    const winningCards = gameBoard?.board.flat().filter((card) => card.isWinning);
-    return winningCards?.length * gameBoard.amountPerWin;
+    return gameBoard.board.flat().filter((card) => card.isWinning).length * gameBoard.amountPerWin;
   }, [gameBoard]);
+
+  const balance: number = useMemo<number>(() => {
+    if (!gameBoard || !exposedCards.current.size) return 0;
+    const exposedLosingCards = Array.from(exposedCards.current).filter((card) => !card.isWinning);
+    if (exposedLosingCards.length > 0) return 0;
+    return exposedCards.current.size * gameBoard.amountPerWin;
+  }, [gameBoard, exposedCards]);
+
+  const gameStatus = useMemo<GameStatus>(() => {
+    if (!gameBoard) return "first-round";
+    const exposedLosingCards = Array.from(exposedCards.current).filter((card) => !card.isWinning);
+    if (exposedLosingCards.length > 0) return "lose";
+    else if (balance === 0 && exposedCards.current.size > 0) return "cashed-out";
+    else if (exposedCards.current.size > 0) return "in-progress";
+    else return "first-round";
+  }, [gameBoard, exposedCards, balance]);
+
+  const handleGameCardExposed = (cardId: number) => {
+    if (gameStatus === "lose" || gameStatus === "cashed-out") return;
+    setGameCardExposed(cardId);
+  };
+
+  const gameBoardFooter = () => {
+    switch (gameStatus) {
+      case "cashed-out":
+      case "lose":
+        return (
+          <BaseButton fullWidth onClick={resetGameBoard}>
+            Play Again
+          </BaseButton>
+        );
+      case "in-progress":
+        return (
+          <BaseButton variant="secondary" fullWidth onClick={() => {}}>
+            Cash Out
+          </BaseButton>
+        );
+    }
+  };
 
   useEffect(() => {
     fetchGameBoard();
@@ -33,8 +73,9 @@ export const GamePage = () => {
             </FramedBox>
           </div>
           <FramedBox contentFrameStyle="square">
-            <GameBoard gameBoard={gameBoard.board} setGameCardExposed={setGameCardExposed} />
+            <GameBoard gameBoard={gameBoard.board} setGameCardExposed={handleGameCardExposed} />
           </FramedBox>
+          {gameStatus !== "first-round" && <div className={styles["game-board-footer"]}>{gameBoardFooter()}</div>}
         </div>
       )}
     </div>
