36b3ddf0c7a2b27c496944303a3d8a1e4c4c81f7
Aviv R <aviv80010@gmail.com>
2025-12-29 23:47:33 +0200

    Refactor AppLogo component by removing redundant CSS class for logo image, simplifying the markup.

--- src/components/AppLogo/AppLogo.tsx
+++ src/components/AppLogo/AppLogo.tsx
@@ -4,7 +4,7 @@ import logoImage from "../../assets/graphics/logo.png";
 export const AppLogo = () => {
   return (
     <div className={styles["app-logo"]}>
-      <img src={logoImage} alt="Lucky Miner Logo" className={styles["logo-image"]} />
+      <img src={logoImage} alt="Lucky Miner Logo" />
     </div>
   );
 };
