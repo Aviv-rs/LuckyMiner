ce2eca3dbf2e6ae06696d253a8344230eac1eb7b
Aviv R <aviv80010@gmail.com>
2025-12-29 07:14:11 +0200

    Added control for losing cards count with min and max values

--- src/mock-server/game.controller.ts
+++ src/mock-server/game.controller.ts
@@ -1,18 +1,35 @@
 import type { GameDto } from "../types/game.types";
-import { generateUniqueId } from "../utils/generate.utils";
+import { generateRandomNumberInRange, generateUniqueId } from "../utils/generate.utils";
+
 const gameBoardSize = 3;
+const minLosingCardsCount = 1;
+const maxLosingCardsCount = 3;
 
 export const gameController = {
   _getRandomBoolean: () => {
     return Math.random() < 0.5;
   },
   _generateBoard: () => {
-    return Array.from({ length: gameBoardSize }, () =>
+    const board = Array.from({ length: gameBoardSize }, () =>
       Array.from({ length: gameBoardSize }, () => ({
         id: generateUniqueId(),
-        isWinning: gameController._getRandomBoolean(),
+        isWinning: true,
       })),
     );
+
+    if (maxLosingCardsCount < minLosingCardsCount) {
+      throw new Error("maxLosingCardsCount must be greater than or equal to minLosingCardsCount");
+    }
+    const losingCardsCount = generateRandomNumberInRange(minLosingCardsCount, maxLosingCardsCount);
+
+    const randomIndices = new Set<number>();
+    while (randomIndices.size < losingCardsCount) {
+      const randomIndex = generateRandomNumberInRange(0, board.length - 1);
+      randomIndices.add(randomIndex);
+      board[randomIndex][randomIndex].isWinning = false;
+    }
+
+    return board;
   },
   getGameBoard: async (): Promise<GameDto> => {
     return new Promise((resolve) => {
--- src/utils/generate.utils.ts
+++ src/utils/generate.utils.ts
@@ -3,3 +3,16 @@ let idCounter = 0;
 export const generateUniqueId = () => {
   return idCounter++;
 };
+
+/**
+ * Generates a random number between min and max (inclusive)
+ * @param min - The minimum number (inclusive)
+ * @param max - The maximum number (inclusive)
+ * @returns A random number between min and max
+ */
+export const generateRandomNumberInRange = (min: number, max: number) => {
+  if (typeof min !== "number" || typeof max !== "number") return 0;
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
